c3d9a9e25237269b644c52da9f94f8abff6ca4e2
julio-valim <juliopvalim@gmail.com>
2023-05-18 21:03:25 -0300

    create folder to order frontend files

---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-700 p-6 text-zinc-200">
+      <h1 className="text-4xl font-bold">GoodTimes</h1>
+      <h2 className="text-2xl italic">Collection</h2>
+    </div>
+  )
+}
